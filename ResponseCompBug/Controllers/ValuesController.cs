37ce34467a93da95814d5251595972001a3cbb1d
Akash Saxena <akash.sax14@gmail.com>
2020-08-12 13:42:11 -0400

    Initial commit

---
+++ ResponseCompBug/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/value
+        [HttpGet(Name = "GetObject")]
+        [HttpHead(Name = "HeadObject")]
+        public IActionResult Get()
+        {
+            string resultjson = "{ \"FirstName\":\"Dwight\",\"LastName\":\"Schrute\" }";
+            var data = Encoding.ASCII.GetBytes(resultjson);
+            var fileResult = new FileContentResult(data, "application/json");
+            return fileResult;
+        }
+    }
+}
